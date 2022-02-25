5d04254c6d19267f5d03a071fd15cf6f35c021a5
amarhasanovic <ahasanovic4@etf.unsa.ba>
2022-02-25 14:39:33 +0100

    run homescreen as building

--- src/ba/unsa/etf/rpr/projekat/Main.java
+++ src/ba/unsa/etf/rpr/projekat/Main.java
@@ -12,15 +12,28 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginScreen.fxml"));
-        LoginScreenController ctrl = new LoginScreenController();
+//        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/LoginScreen.fxml"));
+//        LoginScreenController ctrl = new LoginScreenController();
+//        loader.setController(ctrl);
+//        Parent root = loader.load();
+//        primaryStage.setTitle("Login");
+//
+//        Scene scene = new Scene(root);
+//        scene.setFill(Color.TRANSPARENT);
+//        primaryStage.initStyle(StageStyle.TRANSPARENT);
+//        primaryStage.setScene(scene);
+//        ctrl.setStage(primaryStage);
+//        primaryStage.show();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/HomeScreen.fxml"));
+        HomeScreenController ctrl = new HomeScreenController();
         loader.setController(ctrl);
         Parent root = loader.load();
         primaryStage.setTitle("Login");
 
         Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        //scene.setFill(Color.TRANSPARENT);
+        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.setScene(scene);
         ctrl.setStage(primaryStage);
         primaryStage.show();

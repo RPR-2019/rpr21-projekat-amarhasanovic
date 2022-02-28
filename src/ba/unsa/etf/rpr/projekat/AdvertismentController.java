9bb58671d60ab12f5f792072f60a198ed4ea05e3
amarhasanovic <ahasanovic4@etf.unsa.ba>
2022-02-28 15:52:43 +0100

    advertisment with pictures for demand/offer

--- src/ba/unsa/etf/rpr/projekat/AdvertismentController.java
+++ src/ba/unsa/etf/rpr/projekat/AdvertismentController.java
@@ -1,4 +1,19 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class AdvertismentController {
+    @FXML
+    private ImageView jobImageView;
+    private Image picture;
+
+    @FXML
+    private void initialize(){
+        jobImageView.setImage(picture);
+    }
+    public AdvertismentController(String picture) {
+        this.picture = new Image(picture, true);
+    }
 }

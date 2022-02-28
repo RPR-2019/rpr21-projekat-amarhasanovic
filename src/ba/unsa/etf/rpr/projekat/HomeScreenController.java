a4be13b14d7c1629dcaafee6e1932c3c17665fc5
amarhasanovic <ahasanovic4@etf.unsa.ba>
2022-02-28 15:22:06 +0100

    maximize button

--- src/ba/unsa/etf/rpr/projekat/HomeScreenController.java
+++ src/ba/unsa/etf/rpr/projekat/HomeScreenController.java
@@ -6,12 +6,13 @@ import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.AccessibleAction;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.io.IOException;
 
 public class HomeScreenController {
@@ -20,26 +21,37 @@ public class HomeScreenController {
     private JFXDrawer drawer;
     @FXML
     private JFXHamburger hamburger;
+    @FXML
+    private TilePane tilePane;
+    private Button btnSelektovanaSlika;
 
     @FXML
     public void initialize(){
         try {
-            VBox vbox = FXMLLoader.load(getClass().getResource("/fxml/DrawerContent.fxml"));
-            drawer.setSidePane(vbox);
-            HamburgerBackArrowBasicTransition burgerTask = new HamburgerBackArrowBasicTransition(hamburger);
-            burgerTask.setRate(-1);
-            hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
-                burgerTask.setRate(burgerTask.getRate() * (-1));
-                burgerTask.play();
-                if(drawer.isOpened()){
-                    drawer.close();
-                }else{
-                    drawer.open();
-                }
-            });
+            sideBarSetUp();
+
+            tilePane.getChildren().clear();
+            VBox vbox = FXMLLoader.load(getClass().getResource("/fxml/Advertisment.fxml"));
+            tilePane.getChildren().add(vbox);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void sideBarSetUp() throws IOException {
+        VBox vbox = FXMLLoader.load(getClass().getResource("/fxml/DrawerContent.fxml"));
+        drawer.setSidePane(vbox);
+        HamburgerBackArrowBasicTransition burgerTask = new HamburgerBackArrowBasicTransition(hamburger);
+        burgerTask.setRate(-1);
+        hamburger.addEventHandler(MouseEvent.MOUSE_PRESSED, (e) -> {
+            burgerTask.setRate(burgerTask.getRate() * (-1));
+            burgerTask.play();
+            if(drawer.isOpened()){
+                drawer.close();
+            }else{
+                drawer.open();
+            }
+        });
     }
 
     public void setStage(Stage stage) {
@@ -52,4 +64,5 @@ public class HomeScreenController {
     public void minimizeAction(ActionEvent actionEvent){
         this.stage.setIconified(true);
     }
+    public void maximizeAction(ActionEvent actionEvent) { this.stage.setMaximized(!this.stage.isMaximized()); }
 }

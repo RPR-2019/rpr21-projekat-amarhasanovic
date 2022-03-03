e9666c5f81eb8a780ea64a0df63d6345c4de8e9d
amarhasanovic <ahasanovic4@etf.unsa.ba>
2022-03-03 14:03:45 +0100

    login reading from db and validation

--- src/ba/unsa/etf/rpr/projekat/Controllers/LoginScreenController.java
+++ src/ba/unsa/etf/rpr/projekat/Controllers/LoginScreenController.java
@@ -1,41 +1,80 @@
 package ba.unsa.etf.rpr.projekat.Controllers;
 
+import ba.unsa.etf.rpr.projekat.Beans.AdvertismentModel;
+import ba.unsa.etf.rpr.projekat.Beans.User;
+import com.jfoenix.controls.JFXDecorator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.IOException;
 
 public class LoginScreenController {
     public AnchorPane sideBar;
     private Stage stage;
     private double x = 0, y = 0;
+    private AdvertismentModel advertismentModel = AdvertismentModel.getInstance();
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField password;
 
     public void initialize(){
-        sideBar.setOnMousePressed(mouseEvent -> {
-            x = mouseEvent.getSceneX();
-            y = mouseEvent.getSceneY();
-        });
-
-        sideBar.setOnMouseDragged(mouseEvent -> {
-            stage.setX(mouseEvent.getScreenX() - x);
-            stage.setY(mouseEvent.getScreenY() - y);
-        });
+        screenDragSetUp();
     }
 
     @FXML
-    public void exitAction(ActionEvent actionEvent) {
+    private void logInAction(ActionEvent actionEvent) {
+        User currentUser = advertismentModel.getAllUsers().stream()
+                .filter(user -> user.getUsername().equals(username.getText()))
+                .findAny()
+                .orElse(null);
+        if(currentUser != null && currentUser.getPassword().equals(password.getText())){
+            username.getStyleClass().removeAll("t-field-invalid-input");
+            username.getStyleClass().add("t-field");
+            password.getStyleClass().removeAll("t-field-invalid-input");
+            password.getStyleClass().add("t-field");
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/HomeScreen.fxml"));
+            HomeScreenController ctrl = new HomeScreenController();
+            ctrl.setCurrentUser(currentUser);
+            loader.setController(ctrl);
+            Parent root = null;
+            try {
+                Stage secondaryStage = new Stage();
+                root = loader.load();
+                secondaryStage.setTitle("Home screen");
+
+                JFXDecorator decorator = new JFXDecorator(secondaryStage, root);
+                Scene scene = new Scene(decorator);
+
+                secondaryStage.setScene(scene);
+                ctrl.setStage(secondaryStage);
+                this.stage.hide();
+                secondaryStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else {
+            username.getStyleClass().removeAll("t-field");
+            username.getStyleClass().add("t-field-invalid-input");
+            password.getStyleClass().removeAll("t-field");
+            password.getStyleClass().add("t-field-invalid-input");
+        }
+    }
+    @FXML
+    private void exitAction(ActionEvent actionEvent) {
         System.exit(0);
     }
-
     @FXML
-    public void createAccountAction(ActionEvent actionEvent){
+    private void createAccountAction(ActionEvent actionEvent){
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/RegisterScreen.fxml"));
         RegisterScreenController ctrl = new RegisterScreenController();
         loader.setController(ctrl);
@@ -56,7 +95,16 @@ public class LoginScreenController {
             e.printStackTrace();
         }
     }
-
+    private void screenDragSetUp() {
+        sideBar.setOnMousePressed(mouseEvent -> {
+            x = mouseEvent.getSceneX();
+            y = mouseEvent.getSceneY();
+        });
+        sideBar.setOnMouseDragged(mouseEvent -> {
+            stage.setX(mouseEvent.getScreenX() - x);
+            stage.setY(mouseEvent.getScreenY() - y);
+        });
+    }
     public void setStage(Stage stage) {
         this.stage = stage;
     }
